4d69b899d38a25690a34fd951e915e9c8228e762
Felipe Merenda Izidorio <felipemerenda1@gmail.com>
2023-10-08 10:13:11 -0300

    feat: implement JSON and create his methods

--- src/main/java/json/Json.java
+++ src/main/java/json/Json.java
@@ -1,6 +1,18 @@
 package json;
 
-public class Json {
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonIOException;
 
+public class Json {
+    private static final Gson gson = new GsonBuilder().create();
+
+    public static <T> String toJson(T object) {
+        return gson.toJson(object);
+    }
+
+    public static <T> T fromJson(final String json, Class<T> className) throws JsonIOException {
+        return gson.fromJson(json, className);
+    }
 
 }
