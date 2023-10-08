2586e5979df4ddcd293f2f8e57998a6877341569
Felipe Merenda Izidorio <felipemerenda1@gmail.com>
2023-10-08 10:14:55 -0300

    refactor: change variable serverHostname to serverHostName

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -11,23 +11,23 @@ public class Client {
         System.out.print("Port: ");
         int port = Integer.parseInt(stdIn.readLine());
 
-        String serverHostname = "localHost";
+        String serverHostName = "localHost";
 
         Socket echoSocket = null;
         PrintWriter out = null;
         BufferedReader in = null;
 
         try {
-            echoSocket = new Socket(serverHostname, port);
+            echoSocket = new Socket(serverHostName, port);
             out = new PrintWriter(echoSocket.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(
                     echoSocket.getInputStream()));
         } catch (UnknownHostException e) {
-            System.err.println("Don't know about host: " + serverHostname);
+            System.err.println("Don't know about host: " + serverHostName);
             System.exit(1);
         } catch (IOException e) {
             System.err.println("Couldn't get I/O for "
-                    + "the connection to: " + serverHostname);
+                    + "the connection to: " + serverHostName);
             System.exit(1);
         }
 
