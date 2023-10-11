9fa4b200efabfba1f367ea38a8cbfa29e03116aa
Felipe Merenda Izidorio <felipemerenda1@gmail.com>
2023-10-11 10:50:31 -0300

    fix: change throw exception

--- src/main/java/json/Json.java
+++ src/main/java/json/Json.java
@@ -2,7 +2,7 @@ package json;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
 
 public class Json {
     private static final Gson gson = new GsonBuilder().create();
@@ -11,8 +11,7 @@ public class Json {
         return gson.toJson(object);
     }
 
-    public static <T> T fromJson(final String json, Class<T> className) throws JsonIOException {
+    public static <T> T fromJson(final String json, Class<T> className) throws JsonSyntaxException {
         return gson.fromJson(json, className);
     }
-
 }
