f28a2e9aa91056653b7d1a30a0f2d2a95531f3cd
Felipe Merenda Izidorio <felipemerenda1@gmail.com>
2023-10-16 21:46:44 -0300

    feat: implement the procedure for exchanging information between Client and Server

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,53 +1,204 @@
 package client;
-import java.io.*;
-import java.net.*;
+
+import com.google.gson.JsonSyntaxException;
+import json.Json;
+import json.validation.ConstraintViolated;
+import json.validation.ValidationHelper;
+import protocol.Optional;
+import protocol.request.*;
+import protocol.request.header.Header;
+import protocol.response.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Parameter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 
 public class Client {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        String serverHost = "localhost";
+        int port = 24801;
 
-        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("IP: ");
-        String IPServer = stdIn.readLine();
-        System.out.print("Port: ");
-        int port = Integer.parseInt(stdIn.readLine());
-
-        String serverHostName = "localHost";
-
-        Socket echoSocket = null;
-        PrintWriter out = null;
-        BufferedReader in = null;
-
-        try {
-            echoSocket = new Socket(serverHostName, port);
-            out = new PrintWriter(echoSocket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(
-                    echoSocket.getInputStream()));
+        try (Socket echoSocket = new Socket(serverHost, port);
+             PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(
+                     echoSocket.getInputStream()));
+             BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in))
+        ) {
+            repl(out, in, stdin);
         } catch (UnknownHostException e) {
-            System.err.println("Don't know about host: " + serverHostName);
+            System.err.println("Don't know about host: " + serverHost);
             System.exit(1);
         } catch (IOException e) {
             System.err.println("Couldn't get I/O for "
-                    + "the connection to: " + serverHostName);
+                    + "the connection to: " + serverHost);
             System.exit(1);
         }
+    }
 
-        String userInput;
+    private static void repl(PrintWriter out, BufferedReader in, BufferedReader stdin) {
+        String token = null;
+        try {
+            while (true) {
+                Request<?> request = requestFactory(stdin, token);
+                String jsonRequest = Json.toJson(request);
+                out.println(jsonRequest);
+                System.out.println();
+                System.out.println("Request criado: " + request);
+                System.out.println("Request enviado: " + jsonRequest);
+                System.out.println();
 
-        System.out.println ("Type Message (\"Bye.\" to quit)");
-        while ((userInput = stdIn.readLine()) != null)
-        {
-            out.println(userInput);
+                String jsonResponse = in.readLine();
+                if (jsonResponse == null) {
+                    System.err.println("ERRO: JSON Response nulo");
+                    break;
+                }
 
-            // end loop
-            if (userInput.equals("Bye."))
-                break;
+                System.out.println("Response recebido: " + jsonResponse);
+                Response<?> response = handleResponse(jsonResponse, request);
+                System.out.println("Response criado: " + response);
 
-            System.out.println("echo: " + in.readLine());
+                if (response == null) {
+                    continue;
+                }
+
+                if (response instanceof LoginResponse) {
+                    token = ((LoginResponse) response).payload().getToken();
+                    System.out.println("token was set");
+                }
+
+                if (response instanceof LogoutResponse) {
+                    break;
+                }
+
+                System.out.println();
+
+            }
+        } catch (IOException e) {
+            System.out.println("ERRO: nao foi possivel ler o stdIn");
+        }
+    }
+
+    private static Request<?> requestFactory(BufferedReader stdin, String token) throws IOException {
+
+        String operation;
+        while (true) {
+            System.out.print("Insira a operação: ");
+            operation = stdin.readLine();
+            if (operation == null) {
+                throw new IOException();
+            }
+
+            switch (operation) {
+                case RequestOperations.LOGIN:
+                    return makeRequest(stdin, token, LoginRequest.class);
+                case RequestOperations.LOGOUT:
+                    return makeRequest(stdin, token, LogoutRequest.class);
+                case RequestOperations.ADMIN_BUSCAR_USUARIOS:
+                    return makeRequest(stdin, token, AdminFindUsersRequest.class);
+                case RequestOperations.ADMIN_BUSCAR_USUARIO:
+                    return makeRequest(stdin, token, AdminFindUserRequest.class);
+                case RequestOperations.ADMIN_CADASTRAR_USUARIO:
+                    return makeRequest(stdin, token, AdminCreateUserRequest.class);
+                case RequestOperations.ADMIN_ATUALIZAR_USUARIO:
+                    return makeRequest(stdin, token, AdminUpdateUserRequest.class);
+            }
+        }
+    }
+
+    private static Response<?> handleResponse(String json, Request<?> request) {
+        Response<?> response = null;
+        try {
+            Class<?> clazz = request.getClass();
+            if (clazz == LoginRequest.class) {
+                response = Json.fromJson(json, LoginResponse.class);
+            }
+            if (clazz == LogoutRequest.class) {
+                response = Json.fromJson(json, LogoutResponse.class);
+            }
+            if (clazz == AdminFindUsersRequest.class) {
+                response = Json.fromJson(json, FindUsersResponse.class);
+            }
+            if (clazz == AdminFindUserRequest.class) {
+                response = Json.fromJson(json, FindUserResponse.class);
+            }
+            if (clazz == AdminCreateUserRequest.class) {
+                response = Json.fromJson(json, AdminCreateUserResponse.class);
+            }
+            if (clazz == AdminUpdateUserRequest.class) {
+                response = Json.fromJson(json, AdminUpdateUserResponse.class);
+            }
+
+            if (response == null || response.payload() == null) {
+                response = Json.fromJson(json, ErrorResponse.class);
+            }
+            ValidationHelper.validate(response);
+            return response;
+        } catch (ConstraintViolated e) {
+            System.err.println("ERRO: Resposta invalida.\n" + e.getMessage());
+            return response;
+        } catch (JsonSyntaxException e) {
+            System.err.println("ERRO: JSON invalido.");
+        }
+        return null;
+    }
+
+
+    private static <T> T makeRequest(BufferedReader stdin, String token, Class<T> clazz) throws IOException {
+        for (Constructor<?> constructor : clazz.getConstructors()) {
+            Parameter[] parameters = constructor.getParameters();
+            boolean shouldSkip = false;
+
+            for (Parameter parameter : parameters) {
+                if (parameter.getType() == Header.class) {
+                    shouldSkip = true;
+                    break;
+                }
+            }
+            if (shouldSkip) {
+                continue;
+            }
+
+            Object[] constructorArguments = new Object[parameters.length];
+            for (int i = 0; i < parameters.length; i++) {
+                if (parameters[i].getName().toLowerCase().contains("token")) {
+                    constructorArguments[i] = token;
+                    continue;
+                }
+                System.out.print(parameters[i].getName());
+                if (parameters[i].isAnnotationPresent(Optional.class)) {
+                    System.out.print(" (opcional)");
+                }
+                System.out.print(": ");
+                String line = stdin.readLine();
+                if (line.isBlank() || line.isEmpty()) {
+                    constructorArguments[i] = null;
+                } else if (parameters[i].getType() == Long.class) {
+                    constructorArguments[i] = Long.parseLong(line);
+                } else if (parameters[i].getType() == Integer.class) {
+                    constructorArguments[i] = Integer.parseInt(line);
+                } else if (parameters[i].getType() == Boolean.class) {
+                    constructorArguments[i] = Boolean.parseBoolean(line);
+                } else {
+                    constructorArguments[i] = line;
+                }
+            }
+
+            try {
+
+                return (T) constructor.newInstance(constructorArguments);
+            } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                throw new RuntimeException(e);
+            }
         }
 
-        out.close();
-        in.close();
-        stdIn.close();
-        echoSocket.close();
+        throw new RuntimeException("ERRO: Nao foi possivel criar a instancia: " + clazz.getName());
     }
 }
+
--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,85 +1,87 @@
 package server;
 
+import json.Json;
+import protocol.request.RequestOperations;
+import protocol.response.LogoutResponse;
+import protocol.response.Response;
+import server.exceptions.ServerResponseException;
+import server.procedure.*;
+import server.router.Router;
+
 import java.net.*;
 import java.io.*;
 
-public class Server extends Thread
-{
-    protected Socket clientSocket;
+public class Server extends Thread {
+    private final Socket clientSocket;
+    private Router routes = null;
 
-    public static void main(String[] args) throws IOException
-    {
-        ServerSocket serverSocket = null;
-
-        try {
-            serverSocket = new ServerSocket(24800);
-            System.out.println ("Connection Socket Created");
-            try {
-                while (true)
-                {
-                    System.out.println ("Waiting for Connection");
-                    new Server (serverSocket.accept());
-                }
-            }
-            catch (IOException e)
-            {
-                System.err.println("Accept failed.");
-                System.exit(1);
-            }
-        }
-        catch (IOException e)
-        {
-            System.err.println("Could not listen on port: 10008.");
-            System.exit(1);
-        }
-        finally
-        {
-            try {
-                serverSocket.close();
-            }
-            catch (IOException e)
-            {
-                System.err.println("Could not close port: 10008.");
-                System.exit(1);
-            }
-        }
-    }
-
-    private Server (Socket clientSoc)
-    {
+    private Server(Socket clientSoc) {
         clientSocket = clientSoc;
+        if (routes == null) {
+            routes = Router.builder()
+                    .addRoute(RequestOperations.LOGIN, new LoginProcedure())
+                    .addRoute(RequestOperations.LOGOUT, new LogoutProcedure())
+                    .addRoute(RequestOperations.ADMIN_BUSCAR_USUARIOS, new AdminFindUsersProcedure())
+                    .addRoute(RequestOperations.ADMIN_BUSCAR_USUARIO, new AdminFindUserProcedure())
+                    .addRoute(RequestOperations.ADMIN_CADASTRAR_USUARIO, new AdminCreateUserProcedure())
+                    .addRoute(RequestOperations.ADMIN_ATUALIZAR_USUARIO, new AdminUpdateUserProcedure())
+                    .addRoute(RequestOperations.ADMIN_DELETAR_USUARIO, new AdminDeleteUserProcedure())
+                    .build();
+        }
         start();
     }
 
-    public void run()
+    public static void main(String[] args) throws IOException
     {
-        System.out.println ("New Communication Thread Started");
+        InetAddress ipAddress = InetAddress.getByName("0.0.0.0");
+        final int port = 24801;
+        try(ServerSocket serverSocket = new ServerSocket(port, 0, ipAddress)) {
+            System.out.println("Connection Socket Created");
+            while (true) {
+                try {
+                    System.out.println("Waiting for Connection");
+                    new Server(serverSocket.accept());
+                } catch (IOException e) {
+                    System.err.println("Accept failed.");
+                    System.exit(1);
+                }
+            }
+        } catch (IOException e) {
+            System.err.printf("Could not listen on port: %d.%n", port);
+            System.exit(1);
+        }
+    }
 
-        try {
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),
-                    true);
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader( clientSocket.getInputStream()));
+    public void run() {
+        System.out.println("New Communication Thread Started");
 
+        try (clientSocket;
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        ) {
             String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println("Request Recebido: " + inputLine);
+                Response<?> response;
 
-            while ((inputLine = in.readLine()) != null)
-            {
-                System.out.println ("Server: " + inputLine);
-                out.println(inputLine);
+                try {
+                    response = routes.serve(inputLine);
+                } catch (ServerResponseException e) {
+                    response = e.intoResponse();
+                }
 
-                if (inputLine.equals("Bye."))
+                String jsonResponse = Json.toJson(response);
+                System.out.println("Response Enviado: " + jsonResponse);
+                out.println(jsonResponse);
+
+                if (response instanceof LogoutResponse)
                     break;
             }
-
-            out.close();
-            in.close();
-            clientSocket.close();
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             System.err.println("Problem with Communication Server");
             System.exit(1);
         }
+
+        assert (clientSocket.isClosed());
     }
 }
