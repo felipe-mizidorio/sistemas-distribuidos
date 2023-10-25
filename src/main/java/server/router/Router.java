64b15a1cdd64de89ddcf8cb6341f00fa63639750
Felipe Merenda Izidorio <felipemerenda1@gmail.com>
2023-10-25 10:49:48 -0300

    refactor: change the name for Process

--- src/main/java/server/router/Router.java
+++ src/main/java/server/router/Router.java
@@ -10,15 +10,15 @@ import server.exceptions.BadRequestException;
 import server.exceptions.MethodNotAllowedException;
 import server.exceptions.ServerResponseException;
 import json.validation.ConstraintViolated;
-import json.validation.ValidationHelper;
-import server.procedure.Procedure;
+import json.validation.ValidateJson;
+import server.processes.Process;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @AllArgsConstructor
 public class Router {
-    private Map<String, Procedure> routes;
+    private Map<String, Process> routes;
 
     public static RouterBuilder builder() {
         return new RouterBuilder();
@@ -28,26 +28,26 @@ public class Router {
         EmptyRequest req;
         try {
             req = Json.fromJson(stringRequest, EmptyRequest.class);
-            ValidationHelper.validate(req);
+            ValidateJson.validate(req);
         } catch (JsonSyntaxException e) {
-            throw new BadRequestException("Invalid header");
+            throw new BadRequestException("ERRO: Header inválido");
         } catch (ConstraintViolated e) {
             throw new BadRequestException(e.getMessage());
         }
 
-        var procedure = routes.get(req.getHeader().operation());
-        if (procedure == null) {
+        var operation = routes.get(req.getHeader().operation());
+        if (operation == null) {
             throw new MethodNotAllowedException(req.getHeader().operation());
         }
 
-        return procedure.doProcedure(stringRequest);
+        return operation.execute(stringRequest);
     }
 
     public static class RouterBuilder {
         @NonNull
-        private final Map<String, Procedure> routes = new HashMap<>();
+        private final Map<String, Process> routes = new HashMap<>();
 
-        public RouterBuilder addRoute(String operation, Procedure handler) {
+        public RouterBuilder addRoute(String operation, Process handler) {
             routes.put(operation, handler);
             return this;
         }
