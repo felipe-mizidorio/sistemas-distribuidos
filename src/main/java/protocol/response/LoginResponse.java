7e2d702583c2526f0d8ac96544b8cdc96fdb63a8
Felipe Merenda Izidorio <felipemerenda1@gmail.com>
2023-10-26 13:26:05 -0300

    refactor: add annotations for payload to be valid and not null

--- src/main/java/protocol/response/LoginResponse.java
+++ src/main/java/protocol/response/LoginResponse.java
@@ -1,9 +1,13 @@
 package protocol.response;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 public class LoginResponse implements Response<LoginResponse.Payload> {
+    @NotNull
+    @Valid
     private final Payload payload;
 
     public LoginResponse(final String token) {
@@ -31,7 +35,7 @@ public class LoginResponse implements Response<LoginResponse.Payload> {
         @NotBlank
         private final String token;
 
-        public Payload(String token) {
+        public Payload(final String token) {
             this.token = token;
         }
 
