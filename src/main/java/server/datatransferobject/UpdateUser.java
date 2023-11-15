f48d67595f44831ebd88eb6e2b679b82726839da
Felipe Merenda Izidorio <felipemerenda1@gmail.com>
2023-11-15 15:18:20 -0300

    feat: add name and sender's register variables

--- src/main/java/server/datatransferobject/UpdateUser.java
+++ src/main/java/server/datatransferobject/UpdateUser.java
@@ -5,17 +5,19 @@ import lombok.Getter;
 
 @Getter
 public class UpdateUser {
+    private final long sender;
     private final long registro;
-    private final String email;
     private final String nome;
+    private final String email;
     private final String senha;
     private final Boolean tipo;
 
     @Builder
-    public UpdateUser(long registro, String email, String nome, String senha, Boolean tipo) {
+    public UpdateUser(long sender, long registro, String nome, String email, String senha, Boolean tipo) {
+        this.sender = sender;
         this.registro = registro;
-        this.email = email;
         this.nome = nome;
+        this.email = email;
         this.senha = senha;
         this.tipo = tipo;
     }
