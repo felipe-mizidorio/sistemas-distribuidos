666a0e093d540e3f594395fad111f87c0f790994
Felipe Merenda Izidorio <felipemerenda1@gmail.com>
2023-12-13 10:27:30 -0300

    feat: create controller class for the Segment

--- src/main/java/server/controller/SegmentController.java
+++ src/main/java/server/controller/SegmentController.java
@@ -10,6 +10,7 @@ import server.exceptions.ServerResponseException;
 import server.repository.SegmentRepository;
 
 import java.util.List;
+import java.util.Objects;
 
 public class SegmentController {
     private static SegmentController instance = null;
@@ -43,13 +44,34 @@ public class SegmentController {
     }
 
     public SegmentDTO updateSegment(UpdateSegment segment) throws ServerResponseException {
-        Long segmentId = findSegment(segment.getPdiInicial(), segment.getPdiFinal()).getId();
+        Long segmentId = findSegment(segment.getPdi_inicial(), segment.getPdi_final()).getId();
         var entity = repository.update(segmentId, Segment.of(segment));
         return SegmentDTO.of(entity);
     }
 
     public void deleteSegment(DeleteSegment segment) throws ResourceNotFoundException {
-        Long segmentId = findSegment(segment.getPdiInicial(), segment.getPdiFinal()).getId();
+        Long segmentId = findSegment(segment.getPdi_inicial(), segment.getPdi_final()).getId();
         repository.delete(segmentId);
+    }
+
+    public void deleteSegment(Long id) {
+        repository.delete(id);
+    }
+
+    public List<SegmentDTO> findNeighborsSegments(Long id) {
+        return repository.findNeighborsNodeSegments(id)
+                .stream()
+                .map(SegmentDTO::of)
+                .toList();
+    }
+
+    public Long findNextNodeInSegment(Long idSegment, Long idNode) throws ResourceNotFoundException {
+        Segment segment = repository.find(idSegment).orElseThrow(()->new ResourceNotFoundException("Segmento não encontrado"));
+        if(Objects.equals(segment.getPdi_inicial(), idNode)) {
+            return segment.getPdi_final();
+        } else if(Objects.equals(segment.getPdi_final(), idNode)) {
+            return segment.getPdi_inicial();
+        }
+        return null;
     }
 }
